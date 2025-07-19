58654f9e782bee0df01b238f31876acb3934bade
Black Fighter <blackfighter@gmail.com>
2025-07-19 23:48:25 +0600

    final

--- convertKey.js
+++ convertKey.js
@@ -1,4 +1,3 @@
 const fs = require('fs');
 const key = fs.readFileSync('./my-medi-camp-firebase-adminsdk-fbsvc-2fae8884c4.json', 'utf8');
 const base64 = Buffer.from(key).toString('base64')
-// console.log(base64)
